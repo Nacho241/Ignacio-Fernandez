a17d6ca120cb7257d262e46af2e8b25e2ab3c702
Ignacio Fernandez <fernandezignacio241@gmail.com>
2024-10-31 11:18:20 -0300

    Escrito de Programacion

---
+++ Desktop/Escrito/Models/Propiedad/Propiedad/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Propiedad
+{
+    public int Id { get; set; }
+    public string Tipo { get; set; }
+    public string Direccion { get; set; }
+    public decimal MetrosCuadrados { get; set; }
+    public decimal Precio { get; set; }
+}
---
+++ Desktop/Escrito/Models/Ventas/Ventas/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Venta
+{
+    public int Id { get; set; }
+    public decimal Precio { get; set; }
+    public decimal Iva { get; set; }
+    public decimal Descuento { get; set; }
+    public decimal PrecioTotal { get; set; }
+    public int IdProp { get; set; }
+}
